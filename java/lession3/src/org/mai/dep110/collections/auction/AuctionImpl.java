4a2de8817b9fc8b08136ed747c968b4b97485434
Alexander <ledgergap@gmail.com>
2019-10-02 18:18:22 +0300

    Лабораторная на Unit tests

---
+++ java/lession3/src/org/mai/dep110/collections/auction/AuctionImpl.java
@@ -0,0 +1,39 @@
+package org.mai.dep110.collections.auction;
+
+import org.junit.Test;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+
+public class AuctionImpl implements Auction {
+    @Override
+    public void placeProduct(String product, BigDecimal initialPrice) {
+
+    }
+
+    @Override
+    public void addBid(String user, String product, BigDecimal price) {
+
+    }
+
+    @Override
+    public void removeBid(String user, String product) {
+
+    }
+
+    @Override
+    public boolean sellProduct(String product) {
+        return false;
+    }
+
+    @Override
+    public List<String> getProducts() {
+        return null;
+    }
+
+    @Override
+    public BigDecimal getProductPrice(String product) {
+        return null;
+    }
+}
