105958f795f5ff79615683605091eb7783adb5bd
Alexander <ledgergap@gmail.com>
2019-10-03 14:13:58 +0300

    Лабораторная на Unit tests

--- oracle_db/oltp_test.sql
+++ oracle_db/oltp_test.sql
@@ -1,41 +1,43 @@
---1.Выбрать все заказы (SALES_ORDER)
+1)
 SELECT * FROM sales_order;
---2.Выбрать все заказы, введенные после 1 января 2016 года
-SELECT * FROM sales_order WHERE ORDER_DATE > to_date('01-01-2016','DD-MM-YYYY');
---3.Выбрать все заказы, введенные после 1 января 2016 года и до 15 июля 2016 года
-SELECT * FROM sales_order WHERE ORDER_DATE > to_date('01-01-2016','DD-MM-YYYY')
-                            AND ORDER_DATE < to_date('15-07-2016','DD-MM-YYYY');
---4.Найти менеджеров с именем 'Henry' 
-SELECT * FROM manager WHERE MANAGER_FIRST_NAME = 'Henry';
---5.Выбрать все заказы менеджеров с именем Henry
-SELECT * FROM manager, sales_order 
-    WHERE manager.manager_id = sales_order.manager_id 
-    AND MANAGER_FIRST_NAME = 'Henry';
 
---6.Выбрать все уникальные страны из таблицы CITY
-SELECT DISTINCT country
-    FROM CITY;
---7.Выбрать все уникальные комбинации страны и региона из таблицы CITY
-SELECT DISTINCT country, region
-    FROM CITY;
---8.Выбрать все страны из таблицы CITY с количеством городов в них.
-SELECT COUNT(region), country
-    FROM CITY
-    GROUP BY country;
---9.Выбрать количество товаров (QTY), проданное с 1 по 30 января 2016 года.
-SELECT sale_qty 
-    FROM mv_fact_sale 
-    WHERE sale_date BETWEEN to_date('01-01-2016','DD-MM-YYYY') AND to_date('30-01-2016','DD-MM-YYYY');
---10.Выбрать все уникальные названия городов, регионов и стран в одной колонке
-SELECT region FROM CITY
-	UNION SELECT country FROM CITY
-    UNION SELECT city_name FROM CITY;
+2)
+SELECT * FROM sales_order where ORDER_DATE > to_date('01-01-2016','DD-MM-YYYY');
 
---=================================
-SELECT * FROM manager;        --===
-SELECT * FROM mv_fact_sale;   --===
---=================================
+3)
+SELECT * FROM sales_order where ORDER_DATE > to_date('01-01-2016','DD-MM-YYYY') and ORDER_DATE <to_date('15-07-2016','DD-MM-YYYY');
 
---11.Вывести имена и фамилии менеджер(ов), продавшего товаров в январе 2016 года на наибольшую сумму.
-SELECT manager_first_name, manager_last_name FROM manager;
-    where 
+4)
+SELECT * FROM manager where MANAGER_FIRST_NAME = 'Henry';
+
+5)
+SELECT * FROM SALES_ORDER a, MANAGER b  where a.MANAGER_ID = b.MANAGER_ID and b.manager_first_name = 'Henry';
+
+6)
+SELECT distinct country FROM CITY;
+
+7)
+SELECT distinct country, region FROM CITY;
+
+8)
+SELECT country, count(CITY_NAME) as amount FROM CITY GROUP BY country;
+
+9)
+SELECT a.product_qty,b.order_date  from SALES_ORDER_LINE a, SALES_ORDER b where
+a.sales_order_id = b.sales_order_id and b.order_date > to_date('01-01-2016','DD-MM-YYYY') and
+b.order_date <to_date('30-01-2016','DD-MM-YYYY');
+
+10)
+SELECT region FROM CITY UNION SELECT country FROM CITY UNION SELECT city_name FROM CITY;
+
+11)
+--
+SELECT f.manager_first_name, f.manager_last_name , f.income FROM
+(SELECT  sss.manager_first_name, sss.manager_last_name,  count(sss.product_price*sss.product_qty) as income  FROM (SELECT * FROM  MANAGER a  LEFT OUTER JOIN SALES_ORDER b ON a.manager_id = b.manager_id
+LEFT OUTER JOIN SALES_ORDER_LINE c ON b.sales_order_id = b.sales_order_id) sss
+where sss.ORDER_DATE > to_date('01-01-2016','DD-MM-YYYY')
+and sss.ORDER_DATE < to_date('30-01-2016','DD-MM-YYYY') group by sss.manager_first_name, sss.manager_last_name) f; --where f.income = (SELECT MAX(income) from f);
+
+
+
+
