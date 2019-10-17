d252c61814d6e1a8e8c5368f75d4bdd62b95a278
Василий Щербаков <34073365+vasil68@users.noreply.github.com>
2019-10-17 19:09:50 +0300

    исправил запрос

--- oracle_db/query11.sql
+++ oracle_db/query11.sql
@@ -1,9 +1,9 @@
-with
-(select manager_first_name,manager_last_name ,sum(product_price*product_qty) price
+with managers as (select manager_first_name,manager_last_name ,sum(product_price*product_qty) price
  from manager man join sales_order sale on man.manager_id = sale.manager_id
  join sales_order_line orderline on sale.sales_order_id = orderline.sales_order_id
- where order_date >= '01/01/2016' AND order_date <='31/01/2016'
- group by manager_first_name,manager_last_name) as managers
+ where order_date >= to_date('01/01/2016','DD.MM.YYYY') 
+       AND order_date <= to_date('31/01/2016','DD.MM.YYYY')
+ group by man.manager_id,manager_first_name,manager_last_name)
 select manager_first_name,manager_last_name
-from (select max(price) from managers) maximum, managers
-where managers.price = maximum.price
+from (select max(price) max_price from managers) maximum, managers
+where managers.price = maximum.max_price;
