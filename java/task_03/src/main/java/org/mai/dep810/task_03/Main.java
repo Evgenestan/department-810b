22b058bd55aba087544e6a73258d64bbd58617d9
vasil68 <vaia.punk.rok@gmail.com>
2019-10-21 20:11:42 +0300

    исправлены третья и первая лаба

--- java/task_03/src/main/java/org/mai/dep810/task_03/Main.java
+++ java/task_03/src/main/java/org/mai/dep810/task_03/Main.java
@@ -1,4 +1,6 @@
-package org.mai.dep810.task_03;
+package mai.dep810.task_03;
+
+import mai.dep810.task_03.WebSite;
 
 import java.util.ArrayList;
 import java.util.concurrent.*;
@@ -7,7 +9,39 @@ public class Main
 {
     public static void main(String[] args) throws InterruptedException
     {
-        var webSitesVisits = new WebSite();
+        var pages = new WebSite [5];
+
+        pages [0] = new WebSite("www.google.com");
+        pages [1] = new WebSite("www.facebook.com");
+        pages [2] = new WebSite("www.twitter.com");
+        pages [3] = new WebSite("www.yandex.ru");
+        pages [4] = new WebSite("www.instagram.com");
+
+        var threads = new Thread[10];
+
+        for (int i = 0; i < 10; i++)
+        {
+            threads[i] = new Thread(()->
+            {
+                int pageNum =(int) rnd(5);
+                pages[pageNum].visit();
+            });
+        }
+
+        for (int i = 0; i < 10; i++)
+        {
+            for (int j = 0; j < 50; j++)
+            {
+                threads[i].run();
+            }
+        }
+
+        for (var page : pages)
+        {
+            System.out.println(page);
+        }
+
+        /*var webSitesVisits = new WebSite();
 
         var executor = Executors.newFixedThreadPool(50);
 
@@ -29,6 +63,14 @@ public class Main
         executor.invokeAll(listVisitsCall);
         executor.shutdown();
 
-        System.out.println(webSitesVisits);
+        System.out.println(webSitesVisits);*/
     }
+
+    public static double rnd(final double max)
+    {
+        return Math.random() * max;
+    }
+
+
+
 }
