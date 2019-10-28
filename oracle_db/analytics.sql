564aa7e5ab5e058aea9fd4d3e7bc18f381f356b4
Alexander <ledgergap@gmail.com>
2019-10-28 15:38:23 +0300

    sql 6 7

--- oracle_db/analytics.sql
+++ oracle_db/analytics.sql
@@ -81,25 +81,125 @@ order by speed_size desc
 
 /*  5. Ќайдите вклад в общую прибыль за 2014 год 10% наиболее дорогих товаров и 10% наиболее дешевых товаров.
 ¬ыведите product_id, product_name, total_sale_amount, percent*/
-with t1 as (
-select product_id, product_name, sum(sale_amount) total_sale_amount
+
+with t0 as (
+select product_id, product_name, sale_amount, AVG(sale_price) over(partition by product_id) av_sal
 from V_FACT_SALE
 where to_date(sale_date,'DD.MM.YY') >= to_date('01.01.2014','DD.MM.YY') and to_date(sale_date,'DD.MM.YY') < to_date('01.01.2015','DD.MM.YY')
-group by product_id, product_name
+),
+t1 as (
+select product_id, product_name, sum(sale_amount) total_sale_amount, av_sal
+from t0
+group by product_id, product_name, av_sal
 order by total_sale_amount desc),
 t2 as (
-select product_id, product_name, total_sale_amount,
-sum(total_sale_amount) over() total_sum
+select product_id, product_name, total_sale_amount, av_sal,
+sum(total_sale_amount) over() total_sum,
+sum(av_sal) over() total_av_sum
 --sum(sale_price) over() total_price_sum
-from t1)
-select product_id, product_name, total_sale_amount, total_sum, round(total_sale_amount/total_sum,2) percent
+from t1),
+t3 as(
+select product_id,
+product_name,
+total_sum,
+total_sale_amount,
+round(av_sal 
+/total_av_sum,2)*100 "total_impact%",
+row_number () over (order by av_sal desc) top_sal,
+av_sal,
+total_av_sum,
+count(*) over () count_num
 from t2
---where percent <=0.1 or percent >=0.9;
+order by av_sal desc)
+
+select product_id,product_name, total_sale_amount, round(total_sale_amount/total_sum*100,2) percent
+from t3
+where top_sal <= round(0.1*count_num) or top_sal >= round(0.9*count_num);
+--where rownum <= round(count(*)*0.1) or rownum >= round(count(*)*0.9)
+
+  
+
+ 
+select * from V_FACT_SALE;
+
+ 
+
+/*6.  омпани€ хочет премировать трех наиболее продуктивных (по объему продаж, конечно) менеджеров в каждой стране в 2014 году.
+¬ыведите country, <список manager_last_name manager_first_name, разделенный зап€тыми> которым будет выплачена преми€ */
+
+with t0 as (
+select manager_id, manager_last_name, manager_first_name, country, sale_date, sale_qty,
+SUM(sale_qty) over (partition by country,manager_id order by sale_qty) country_sale_qty
+from V_FACT_SALE
+where to_date(sale_date,'DD.MM.YY') >= to_date('01.01.2014','DD.MM.YY') and to_date(sale_date,'DD.MM.YY') < to_date('01.01.2015','DD.MM.YY')
+order by country ),
+t1 as (
+select  
+distinct 
+last_value(country_sale_qty ) over (partition by country, manager_id) manager_sum,
+manager_id, manager_last_name, manager_first_name, country
+from t0),
+t2 as (select
+manager_sum,
+manager_id, manager_last_name, manager_first_name, country,
+rank() over (partition by country order by manager_sum) rank_id
+from t1)
+select country, manager_last_name|| ' , ' ||manager_first_name name
+from t2
+where rank_id = 1 or rank_id = 2 or rank_id = 3;
 
 
 
 
-select * from V_FACT_SALE
+/*7. ¬ыведите самый дешевый и самый дорогой товар, проданный за каждый мес€ц в течение 2014 года.
+cheapest_product_id, cheapest_product_name, expensive_product_id, expensive_product_name, month, cheapest_price, expensive_price*/
+
+
+select * from V_FACT_SALE;
+
+with t0 as (
+select product_name,
+product_id,
+sale_price,
+trunc(MONTHS_BETWEEN(sale_date,to_date('01.12.13','DD.MM.YY'))) month_order
+from V_FACT_SALE
+where to_date(sale_date,'DD.MM.YY') >= to_date('01.01.2014','DD.MM.YY') and to_date(sale_date,'DD.MM.YY') < to_date('01.01.2015','DD.MM.YY')
+ ),
+ t1 as(
+select 
+product_id,
+product_name,
+month_order,
+sale_price,
+max (sale_price) over (partition by month_order) max_cost_prod,
+min (sale_price) over (partition by month_order) min_cost_prod
+from t0 )
+select 
+product_id,
+product_name,
+month_order,
+sale_price,
+max_cost_prod,
+min_cost_prod,
+rank() over (partition by month_order order by sale_price desc) rank_id
+from t1
+where max_cost_prod = sale_price or min_cost_prod = sale_price;
+
+
+
+ 
+ 
+
+
+
+
+
+
+
+
+
+
+
 
 
 
