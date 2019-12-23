a77a7b3118bf5259646e9338e0d2179ad8b3052d
Alexander <ledgergap@gmail.com>
2019-12-23 15:26:08 +0300

    C++/ 2,3 task ready. 2 loss 0.06, 3 - improve

--- parallel_cpp/course_work/work_01/optimizator.h
+++ parallel_cpp/course_work/work_01/optimizator.h
@@ -22,12 +22,13 @@ struct Optimizer{
     double lambda, residual, delta;
     double epsilon;
     int epoch, step;
-    int arrow;
+    int x_arrow, y_arrow, z_arrow;
     std::vector<Atom> Pool;
     ParamsArray features;
     double Min_len;
     double min_func;
-    
+    int task_type;
+    int id2,id3_1, id3_2;
     //double residue;
     Optimizer(){};
 
@@ -42,14 +43,17 @@ struct Optimizer{
         double  _multy,
         double  _e_target_i,
         int _x_arrow,
+        int _y_arrow,
+        int _z_arrow,
         int _step,
         int _epoch,
         double _lambda,
         double _residual,
         double _e_coh_B,
         double _epsilon,
-        double _delta):
-
+        double _delta,
+        int _task_type):
+        
     e_coh_i(_e_coh_i),
     B_i(_B_i),
     C11_i(_C11_i),
@@ -60,17 +64,63 @@ struct Optimizer{
     features(_feature),
     Min_len(_Min_len),
     multy(_multy),
-    arrow(_x_arrow),
+    x_arrow(_x_arrow),
+    y_arrow(_y_arrow),
+    z_arrow(_z_arrow),
     step(_step),
     epoch(_epoch),
     lambda(_lambda),
     residual(_residual),
     e_coh_B(_e_coh_B),
     epsilon(_epsilon),
-    delta(_delta)
+    delta(_delta),
+    task_type(_task_type)
     {
 
+        if(task_type == 2){
+            auto find_atom = vector(this->Min_len/sqrt(2),this->Min_len/sqrt(2),0);
+        
+        int m;
+        for(m = 0; m<this->Pool.size(); ++m){
+            if(this->Pool[m].vec == find_atom){
+                
+                break;
+            }
+        }
+        id2 = m;
+        }
+        if(task_type ==3){
+            
 
+        auto find_atom_1_surf = vector(0,0,this->multy*(this->z_arrow)/2.0);
+        auto find_atom_2_surf = vector(this->Min_len/sqrt(2),this->Min_len/sqrt(2),this->multy*(this->z_arrow)/2.0);
+
+        int m;
+        int m1,m2;
+        bool f1 = false;
+        bool f2 = false;
+        for(m = 0; m<this->Pool.size(); ++m){
+            if((f1 && f2)){
+                break;
+            }
+            if(this->Pool[m].vec == find_atom_1_surf){
+                //std::cout<<"m1"<<std::endl;
+                m1 = m;
+                f1 = true;
+                continue;
+            }
+
+            if(this->Pool[m].vec == find_atom_2_surf){
+                //std::cout<<"m2"<<std::endl;
+                m2 = m;
+                f2 = true;
+                continue;
+            }
+        }
+        id3_1 = m1;
+        id3_1 = m2;
+        }
+        
     };
 
     inline double error_function(double & e_coh,
@@ -109,7 +159,7 @@ double optimizer_Huk_Jivs_beta(
 
 double distance(const vector& lv,
     const vector& rv, 
-    const double & length, 
+    const double * array_multy, 
     const double * matrix,
     int size =3);
 
@@ -117,7 +167,7 @@ double E_b(
     Atom const  &elem,
     std::vector <Atom> const &field,
     double const &min_len,
-    const double & multy,
+    const double *array_multy,
     const double* matrix,
     const int & size,
     const ParamsArray& feature);
@@ -127,7 +177,7 @@ double E_r(
     Atom const & elem,
     std::vector <Atom> const  &field,
     double const  &min_len,
-    const double& multy,
+    const double *array_multy,
     const double * matrix,
     const int & size,
     const ParamsArray& feature
@@ -136,7 +186,7 @@ double E_r(
 double E_c(
     std::vector <Atom> const &field,
     double const &min_len,
-    const double & multy,
+    const double *array_multy,
     const double *matrix,
     const int & size,
     const ParamsArray& feature);
@@ -144,7 +194,11 @@ double E_c(
 std::vector<Atom> generate_edge( 
     std::vector<Atom>  &Field,
     const nlohmann::json &file_read,
-    std::string const & path_to);
+    std::string const & path_to,
+    const double & _a,
+    const int &_x,
+    const int & _y,
+    const int & _z);
 
 bool operator == (
     const Atom & left,
